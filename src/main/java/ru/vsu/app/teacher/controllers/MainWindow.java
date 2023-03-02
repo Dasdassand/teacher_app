f4daa56db2a0edd3bb88df048f6c3596e06e4007
Dasdassand <dasdassand@gmail.com>
2023-03-02 10:48:37 +0300

    fix

--- src/main/java/ru/vsu/app/teacher/controllers/MainWindow.java
+++ src/main/java/ru/vsu/app/teacher/controllers/MainWindow.java
@@ -1,25 +1,48 @@
 package ru.vsu.app.teacher.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class MainWindow {
 
     @FXML
-    private ChoiceBox<?> platoon;
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private Button platoonButton;
 
     @FXML
-    private TableView<?> students;
+    private ChoiceBox<?> platoons;
+
+    @FXML
+    private TableColumn<?, ?> students;
 
     @FXML
     private Button studentsButton;
 
     @FXML
+    private TableView<?> tableStudent;
+
+    @FXML
     private Button test;
+
+    @FXML
+    void initialize() {
+        assert platoonButton != null : "fx:id=\"platoonButton\" was not injected: check your FXML file 'Untitled'.";
+        assert platoons != null : "fx:id=\"platoons\" was not injected: check your FXML file 'Untitled'.";
+        assert students != null : "fx:id=\"students\" was not injected: check your FXML file 'Untitled'.";
+        assert studentsButton != null : "fx:id=\"studentsButton\" was not injected: check your FXML file 'Untitled'.";
+        assert tableStudent != null : "fx:id=\"tableStudent\" was not injected: check your FXML file 'Untitled'.";
+        assert test != null : "fx:id=\"test\" was not injected: check your FXML file 'Untitled'.";
+
+    }
 
 }
